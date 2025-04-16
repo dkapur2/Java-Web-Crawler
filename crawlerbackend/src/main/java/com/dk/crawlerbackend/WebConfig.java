8071a3bfe40521aaa170739419c00aa42b5f59ec
Dhruv Kapur <98486791+dkapur2@users.noreply.github.com>
2025-04-16 02:54:01 -0400

    Update WebConfig.java

--- crawlerbackend/src/main/java/com/dk/crawlerbackend/WebConfig.java
+++ crawlerbackend/src/main/java/com/dk/crawlerbackend/WebConfig.java
@@ -1,3 +1,10 @@
+package com.dk.crawlerbackend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class WebConfig {
 

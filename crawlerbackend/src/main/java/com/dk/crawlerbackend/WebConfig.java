50210bc35858f0054533105960d013fc221bfc57
Dhruv Kapur <98486791+dkapur2@users.noreply.github.com>
2025-04-16 02:53:22 -0400

    Update WebConfig.java

--- crawlerbackend/src/main/java/com/dk/crawlerbackend/WebConfig.java
+++ crawlerbackend/src/main/java/com/dk/crawlerbackend/WebConfig.java
@@ -1,10 +1,3 @@
-package com.dk.crawlerbackend;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class WebConfig {
 
@@ -13,11 +6,11 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                       registry.addMapping("/web-crawler/api/**")
+                registry.addMapping("/web-crawler/api/**")
                         .allowedOrigins("https://dkapur.com")
                         .allowedMethods("GET", "POST", "OPTIONS")
-                        .allowedHeaders("*");
-                        .allowCredentials(false); // Change to true if needed
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
             }
         };
     }
